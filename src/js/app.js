54187d348ac006b79f63198ddbe94314332e31de
MyCode07 <erem19992015@mail.ru>
2023-02-13 17:36:46 +0300

    slider change

--- src/js/app.js
+++ src/js/app.js
@@ -1,8 +1,7 @@
 import './utils/smoothscroll.js';
 import './utils/gsapTo.js';
 import './utils/inputmask.js';
+import './utils/slider.js';
 import './utils/home.js';
 import './utils/menu.js';
 import './utils/form.js';
-
-
